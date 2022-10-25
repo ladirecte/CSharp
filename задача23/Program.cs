eef5d1c8e2ff3eb912c31f099cdd83439bd87cd7
Olga <ladirecte@gmail.com>
2022-10-25 18:58:24 +0300

    задача23

---
+++ задача23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает 
+//на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N
+
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int num)
+{
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"{i,4}  {i*i*i,4}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите значенте больше 0");
+    }
+}
+
+SquareTable(n);
