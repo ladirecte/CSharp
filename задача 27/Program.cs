51299e88cb8c765b67477afe2293b2966cd14cb5
Olga <ladirecte@gmail.com>
2022-11-13 15:50:05 +0300

    задача27

--- задача 27/Program.cs
+++ задача 27/Program.cs
@@ -10,7 +10,6 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 int SumDigit(int numDigit)
 {
-    //numDigit = Math.Abs(numDigit); //для отрицательных чисел
     int sum = 0;
     for (int i = 1; i <= num; i++)
     {
@@ -21,3 +20,17 @@ int SumDigit(int numDigit)
 }
 int sumDigit = SumDigit(num);
 Console.WriteLine($"сумма цифр в числе = {sumDigit}");
+
+// не работает((
+//int SumDigit(int num)
+//{
+//  int sum = 0;
+//while (num > 0)
+//{
+//  num /= 10;
+//sum++;
+//}
+//return sum;
+//}
+//int sumDigit = SumDigit(num);
+//Console.WriteLine($"сумма цифр в числе = {sumDigit}");
